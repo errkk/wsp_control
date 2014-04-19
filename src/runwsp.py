201f5b295dbbdf5946a1bf955723ab9bc59b1a16
Eric George <errkkgeorge@gmail.com>
2014-04-19 13:30:38 +0000

    Adding pusher and pushco for notifications

--- src/runwsp.py
+++ src/runwsp.py
@@ -1,12 +1,19 @@
 #! /usr/bin/python
+import pusher
 import sys
 import time
+import requests
 import RPi.GPIO as GPIO
 from datetime import datetime
 
 from config import (PIN, UPLIFT_THRESHOLD, TEMP_CHECK_INTERVAL, PROBE_IN,
                     PROBE_OUT, PROBE_AIR)
+from local_config import PUSHER_CONFIG, PUSHCO_SECRET, PUSHCO_KEY
 from models import Pump, DataLog, FlowMeter, Thermometer
+
+PUSHCO_URL = 'https://api.push.co/1.0/push'
+
+pu = pusher.Pusher(**PUSHER_CONFIG)
 
 GPIO.setwarnings(False)
 # Choose numbering scheme
@@ -24,6 +31,10 @@ probe_in = Thermometer(*PROBE_IN)
 probe_out = Thermometer(*PROBE_OUT)
 probe_air = Thermometer(*PROBE_AIR)
 
+requests.post(PUSHCO_URL, params={'api_key': PUSHCO_KEY,
+    'api_secret': PUSHCO_SECRET,
+    'message': 'WSP Monitor Starting'})
+
 try:
     while True:
         # Make a reading and record it
@@ -37,10 +48,33 @@ try:
         uplift = temp_out - temp_in
         if uplift >= UPLIFT_THRESHOLD:
             print 'Off {0}'.format(uplift)
-            p.turn_on()
+            if p.turn_on():
+                try:
+                    requests.post(PUSHCO_URL, params={'api_key': PUSHCO_KEY,
+                        'api_secret': PUSHCO_SECRET,
+                        'message': 'Pump ON {0}'.format(uplift)})
+                except:
+                    pass
+
+                try:
+                    pu['pump'].trigger('on', uplift)
+                except:
+                    pass
+
         else:
             print 'Off {0}'.format(uplift)
-            p.turn_off()
+            if p.turn_off():
+                try:
+                    requests.post(PUSHCO_URL, params={'api_key': PUSHCO_KEY,
+                        'api_secret': PUSHCO_SECRET,
+                        'message': 'Pump OFF {0}'.format(uplift)})
+                except:
+                    pass
+
+                try:
+                    pu['pump'].trigger('off', uplift)
+                except:
+                    pass
 
         time.sleep(TEMP_CHECK_INTERVAL)
 except KeyboardInterrupt:
