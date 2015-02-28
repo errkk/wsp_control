85ba97d5b2724a4bbc3390f981b9b2f9d7b2d6ad
Eric George <eric@pokelondon.com>
2015-02-28 16:51:14 +0000

    console_scripts

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ setup(
     author_email = "errkkgeorge@gmail.com",
     url = "https://wottonpool.co.uk",
     package_dir={'wsp_control': 'src'},
-    install_requires=['RPi.GPIO', 'redis', 'gspread', 'raven'],
+    install_requires=['RPi.GPIO', 'requests'],
     scripts=['src/runwsp.py'],
     entry_points = {
-        'my_ep_group_id': [
-            'check = wsp_control.functions:check',
-            'toggle = wsp_control.functions:toggle',
-            'override = wsp_control.functions:override',
+        'console_scripts': [
+            'wcheck = wsp_control.functions:check',
+            'wtoggle = wsp_control.functions:toggle',
+            'woverride = wsp_control.functions:override',
         ]
     },
 )
